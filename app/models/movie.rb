66a7216314683c8f78157a7648b721155829d965
GladysKigen <gladys.kigen@student.moringa.com>
2023-03-07 23:36:20 +0300

    complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
     # find all the movies fo a given year
-    def self.find_all_movies_by_year(year)
+     def self.find_all_movies_by_year(year)
         Movie.where(year: year)
       end
     
@@ -30,15 +30,35 @@ class Movie < ActiveRecord::Base
       end
       
       #Can return a movie from the table based on its attributes with the .find_by method
-      def self.find_movie_with_attribute(attributes)
+      def self.find_movie_with_attributes(attributes)
         Movie.find_by(attributes)
       end
 
       #Can use a .where clause to select the appropriate movies released after 2002
-      def self.find_movie_after_2002
-        Movie.where('release_date >2002')
+      def self.find_movies_after_2002
+        Movie.where('release_date > 2002')
       end
 
-      
+      #update
+      #Can update a single movie using the #update method
+      def update_with_attributes(attributes)
+      self.update(attributes)
+      end
+
+      #Can update the title of all records at once using the .update method
+      def self.update_all_titles(title)
+        Movie.update(title: title)
+      end
+
+      #delete
+      #Can delete a single item with the #destroy method
+      def self.delete_by_id(id)
+        Movie.find(id).destroy
+      end
+
+     #Can delete all items at once with the .destroy_all method
+     def self.delete_all_movies
+        Movie.destroy_all
+     end
 
     end
